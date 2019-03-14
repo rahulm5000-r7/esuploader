52329d035f7e28abf2f43479c764ab8ac212bc33
Rahul Monga <rahul_monga@rapid7.com>
2019-03-13 20:52:41 -0500

    use semaphore for concurrency

--- src/main/java/Uploader.java
+++ src/main/java/Uploader.java
@@ -13,35 +13,28 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.unit.ByteSizeUnit;
 import org.elasticsearch.common.unit.ByteSizeValue;
 import org.elasticsearch.common.unit.TimeValue;
-
-import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Semaphore;
 import java.util.function.BiConsumer;
 
 public class Uploader {
     private static Logger logger = LogManager.getLogger(Uploader.class);
     private static final List<String> PLATFORMS = Arrays.asList("windows", "linux", "apache", "unix");
-    private static final List<String> RESULTS = Arrays.asList("PASS", "FAIL", "NOT_APPLICABLE");
+    private static final List<String> RESULTS = Arrays.asList("1", "2", "3");
     private static final Random random = new Random();
-    private static final ExecutorService executor = Executors.newFixedThreadPool(10);
 
+    Semaphore concurrentRequests = new Semaphore(200000);
     private BulkProcessor processor;
     private RestHighLevelClient client;
 
     public Uploader() {
-        client = new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost("<<>>")));
+        client = new RestHighLevelClient(RestClient.builder(HttpHost.create("<<>>")));
 
         BulkProcessor.Listener listener = new BulkProcessor.Listener() {
             @Override
             public void beforeBulk(long executionId, BulkRequest request) {
                 int numberOfActions = request.numberOfActions();
-                logger.info("Executing bulk [{}] with {} requests",
-                             executionId, numberOfActions);
+                logger.info("Executing bulk [{}] with {} requests", executionId, numberOfActions);
             }
 
             @Override
@@ -50,60 +43,38 @@ public class Uploader {
                 if (response.hasFailures()) {
                     logger.info("Bulk [{}] executed with failures", executionId);
                 } else {
-                    logger.info("Bulk [{}] completed in {} milliseconds",
-                                 executionId, response.getTook().getMillis());
+                    logger.info("Bulk [{}] completed in {} milliseconds", executionId, response.getTook().getMillis());
                 }
+                concurrentRequests.release(request.numberOfActions());
             }
 
             @Override
             public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
                 logger.error("Failed to execute bulk", failure);
                 logger.error(failure.getLocalizedMessage() + failure.getMessage());
+                concurrentRequests.release(request.numberOfActions());
             }
         };
 
-        BiConsumer<BulkRequest, ActionListener<BulkResponse>> bulkConsumer =
-            (request, bulkListener) -> client.bulkAsync(request, RequestOptions.DEFAULT, bulkListener);
-
+        BiConsumer<BulkRequest, ActionListener<BulkResponse>> bulkConsumer = (request, bulkListener) -> client.bulkAsync(request, RequestOptions.DEFAULT, bulkListener);
         BulkProcessor.Builder builder = BulkProcessor.builder(bulkConsumer, listener);
-        builder.setBulkActions(2000);
-        builder.setBulkSize(new ByteSizeValue(90L, ByteSizeUnit.MB));
-        builder.setConcurrentRequests(50);
+        builder.setBulkActions(-1);
+        builder.setBulkSize(new ByteSizeValue(20L, ByteSizeUnit.MB));
+        builder.setConcurrentRequests(25);
         builder.setFlushInterval(TimeValue.timeValueSeconds(10L));
         builder.setBackoffPolicy(BackoffPolicy.constantBackoff(TimeValue.timeValueSeconds(1L), 3));
         processor = builder.build();
     }
 
-    public void start() throws IOException, InterruptedException
+    public void start() throws InterruptedException
     {
-        Long start = System.currentTimeMillis();
-        for(int x = 0; x < Settings.ORG_COUNT; x++) {
-            Runnable worker = new OrgAssetUploader(x);
-            executor.execute(worker);
-        }
-        executor.shutdown();
-        while (!executor.isTerminated()) {}
-        processor.awaitClose(3L, TimeUnit.DAYS);
-        processor.close();
-        System.out.println("\nFinished all threads");
-        client.close();
-        Long end = System.currentTimeMillis();
-        System.out.println("Total runtime: " + (end - start) / 1000 + " seconds");
-    }
-
-    private class OrgAssetUploader implements Runnable {
-
-        private int orgId;
-
-        public OrgAssetUploader(int orgId) {
-           this.orgId = orgId;
-        }
-
-        public void run() {
+        for(int orgId = 0; orgId < Settings.ORG_COUNT; orgId++) {
             for(int assetId = 0; assetId < Settings.ASSET_PER_ORG_COUNT; assetId++) {
                 String assetIdentifier = "org_id_" + orgId + "_asset_" + assetId;
 
                 for(int policyId = 0; policyId < Settings.POLICY_PER_ASSET_COUNT; policyId++) {
+                    concurrentRequests.acquire();
+
                     Map<String, Object> policy = new HashMap<>();
                     List<Map<String, Object>> results = new LinkedList<>();
                     policy.put("applicable", random.nextBoolean());
